fb6e64bf246f518e03e7e260359388daccf1a060
Eric Miranda <eric.miranda.ext@siemens-energy.com>
2025-01-07 11:37:49 +0530

    complete cat

--- cat/main.go
+++ cat/main.go
@@ -10,11 +10,12 @@ import (
 func main() {
 	var f *os.File
 	isLineNo := flag.Bool("n", false, "Show line numbers")
+	ignNewline := flag.Bool("b", false, "Ignore newline numbering")
 	flag.Parse()
 
 	if os.Args[1] == "-" {
 		f = os.Stdin
-		catFile(f, *isLineNo, 0)
+		catFile(f, *isLineNo, 0, *ignNewline)
 		return
 	}
 
@@ -22,14 +23,19 @@ func main() {
 	fnames := os.Args[1:]
 	for _, fname := range fnames {
 		f, _ = os.Open(fname)
-		lineNo = catFile(f, *isLineNo, lineNo)
+		lineNo = catFile(f, *isLineNo, lineNo, *ignNewline)
 	}
 }
 
-func catFile(f *os.File, isLine bool, lineNo int) int {
+func catFile(f *os.File, isLine bool, lineNo int, ignoreNewline bool) int {
 	sc := bufio.NewScanner(f)
 
 	for sc.Scan() {
+		if sc.Text() == "" && ignoreNewline {
+			fmt.Println()
+			continue
+		}
+
 		lineNo++
 		if isLine {
 			fmt.Printf("%v ", lineNo)
